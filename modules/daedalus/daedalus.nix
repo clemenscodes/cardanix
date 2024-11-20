febd835d5cd805b29b51afdfe3cfb2aab7c08658
Clemens Horn <clemens.horn@mni.thm.de>
2024-11-20 14:37:12 +0100

    test(nix): syntax

--- modules/daedalus/daedalus.nix
+++ modules/daedalus/daedalus.nix
@@ -6,34 +6,30 @@
 }: let
   daedalus_ = inputs.daedalus.packages.${system}.default;
   daedalusWrapper = pkgs.writeShellScriptBin "daedalus-wrapper" ''
-    XDG_DATA_HOME="${home}"
-    if [ -z "${home}" ]; then
-      XDG_DATA_HOME="$HOME/.local/share"
-    fi
+    XDG_DATA_HOME="''${XDG_DATA_HOME}:-${home}"
     exec ${daedalus_}/bin/daedalus
-  '';
-  desktopEntry = pkgs.writeText "Daedalus.desktop" ''
-    [Desktop Entry]
-    Categories=Application;Network
-    Exec=${daedalus}/bin/daedalus
-    GenericName=Crypto-Currency Wallet
-    Icon=${daedalus}/share/icon_large.png
-    Name=Daedalus mainnet
-    StartupWMClass=Daedalus Mainnet
-    Type=Application
-    Version=1.4
   '';
   daedalus = pkgs.stdenv.mkDerivation {
     inherit (daedalus_) name;
     phases = "installPhase";
     installPhase = ''
-      mkdir -p $out/{bin}
+      mkdir -p $out/{bin,share/applications}
       cp -r ${daedalus_}/config $out/config
       cp -r ${daedalus_}/libexec $out/libexec
-      cp -r ${daedalus_}/share $out/share
       cp ${desktopEntry} $out/share/applications/Daedalus-mainnet.desktop
       cp ${daedalusWrapper}/bin/daedalus-wrapper $out/bin/daedalus
     '';
   };
+  desktopEntry = pkgs.writeText "Daedalus.desktop" ''
+    [Desktop Entry]
+    Categories=Application;Network
+    Exec=${daedalus}/bin/daedalus
+    GenericName=Crypto-Currency Wallet
+    Icon=${daedalus_}/share/icon_large.png
+    Name=Daedalus mainnet
+    StartupWMClass=Daedalus Mainnet
+    Type=Application
+    Version=1.4
+  '';
 in
   daedalus
